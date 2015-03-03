e3c9625dfffab13678b6e908a17e7a64999f3cfd
Atila <atila_sarvari@hotmail.com>
2015-03-03 17:04:52 +0100

    New corners with coalision

--- Project ToiletGames/Assets/GameSetup.js
+++ Project ToiletGames/Assets/GameSetup.js
@@ -2,22 +2,22 @@
 
 var mainCam : Camera;
 
-var topWall : BoxCollider2D;
-var bottomWall : BoxCollider2D;
-var leftWall : BoxCollider2D;
-var rightWall : BoxCollider2D;
+//var topWall : BoxCollider2D;
+//var bottomWall : BoxCollider2D;
+//var leftWall : BoxCollider2D;
+//var rightWall : BoxCollider2D;
 
 function Start () {
-	topWall.size = new Vector2 (mainCam.ScreenToWorldPoint (new Vector3 (Screen.width * 2f, 0f, 1f)).x, 1f);
-	topWall.center = new Vector2 (0f, mainCam.ScreenToWorldPoint (new Vector3 ( 0f, Screen.height, 1f)).y + 0.5f);
-	
-	bottomWall.size = new Vector2 (mainCam.ScreenToWorldPoint (new Vector3 (Screen.width * 2, 0f, 1f)).x, 1f);
-	bottomWall.center = new Vector2 (0f, mainCam.ScreenToWorldPoint (new Vector3( 0f, 0f, 1f)).y - 0.5f);
-	
-	leftWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height*2f,1f)).y);;
-	leftWall.center = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x - 0.5f, 1f);
-	
-	rightWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height*2f, 1f)).y);
-	rightWall.center = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 0.5f, 1f);
+//	topWall.size = new Vector2 (mainCam.ScreenToWorldPoint (new Vector3 (Screen.width * 2f, 0f, 1f)).x, 1f);
+//	topWall.center = new Vector2 (0f, mainCam.ScreenToWorldPoint (new Vector3 ( 0f, Screen.height, 1f)).y + 0.5f);
+//	
+//	bottomWall.size = new Vector2 (mainCam.ScreenToWorldPoint (new Vector3 (Screen.width * 2, 0f, 1f)).x, 1f);
+//	bottomWall.center = new Vector2 (0f, mainCam.ScreenToWorldPoint (new Vector3( 0f, 0f, 1f)).y - 0.5f);
+//	
+//	leftWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height*2f,1f)).y);;
+//	leftWall.center = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x - 0.5f, 1f);
+//	
+//	rightWall.size = new Vector2(1f, mainCam.ScreenToWorldPoint(new Vector3(0f, Screen.height*2f, 1f)).y);
+//	rightWall.center = new Vector2(mainCam.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 0.5f, 1f);
 
 }
